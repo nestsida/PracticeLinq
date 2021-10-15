879d6c8916da731a025ad98f3245c96da0fd6ac8
sarah sida <nestsida@gmail.com>
2021-10-15 09:12:30 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PracticeLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var videoGames = new List<string>();
+            {
+                videoGames.Add("Pokemon");
+                videoGames.Add("Frozen");
+                videoGames.Add("The bear went over the mountain");
+                videoGames.Add("She dances in the rain");
+                videoGames.Add("no need for games");
+                videoGames.Add("");
+            };
+            videoGames.Where(videoGames => videoGames.Contains(""));
+            Console.WriteLine("List all the video games: {videoGames}");
+            Console.WriteLine("This video game has a long name.");
+        }
+    }
+}
